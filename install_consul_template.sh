bc8f5196a69801dc468da78a54590b8930569adc
Michael Wiencek <mwtuea@gmail.com>
2016-09-09 15:17:19 -0500

    Move runit to separate script
    In case you don't need consul-template.

--- install_consul_template.sh
+++ install_consul_template.sh
@@ -6,29 +6,12 @@
 set -e
 cd /root
 
+install_runit.sh
+
 export DEBIAN_FRONTEND=noninteractive
-apt-get update
-apt-get install -y --no-install-recommends \
-    ca-certificates \
-    curl \
-    git \
-    runit \
-    unzip
+apt-get install -y --no-install-recommends unzip
 
-# Install go
-GO_RELEASE=go1.7.linux-amd64.tar.gz
-curl -O https://storage.googleapis.com/golang/$GO_RELEASE
-tar xvf $GO_RELEASE
-chown -R root:root go
-mv go /usr/local/
-
-# Install runsvinit
-export GOPATH=/root/go
-mkdir -p $GOPATH
-/usr/local/go/bin/go get -u github.com/peterbourgon/runsvinit
-cp $GOPATH/bin/runsvinit /usr/local/bin/
-
-## Install consul-template
+# Install consul-template
 CT_VERSION=0.15.0
 CT_RELEASE="consul-template_${CT_VERSION}_linux_amd64.zip"
 
@@ -60,7 +43,4 @@ EOF
 # Cleanup
 rm -rf \
     $CT_RELEASE \
-    $GO_RELEASE \
-    $GOPATH \
-    /usr/local/go \
     /var/lib/apt/lists/*
