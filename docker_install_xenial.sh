458309ab466c54651bc90addc8c0a748914afc35
Laurent Monin <github@norz.org>
2019-04-11 15:48:31 +0200

    Update install procedure for new docker versions

--- docker_install_xenial.sh
+++ docker_install_xenial.sh
@@ -1,32 +1,67 @@
 #!/bin/bash
 
-# install docker
-wget -qO- https://get.docker.io/ | sh
+# Remove old docker install:
+sudo apt-get remove docker docker-engine docker.io containerd runc
+
+# Remove previous old version configs:
+sudo mv -f /etc/systemd/system/docker.service.d/systemd_docker.conf /root/etc_systemd_system_docker.service.d_systemd_docker.conf-$(date --iso-8601=seconds)
+sudo mv -f /etc/default/docker /root/etc_default_docker-$(date --iso-8601=seconds)
+
+# Install few requirements:
+
+sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+
+# Add docker repo:
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+# List available docker versions:
+
+sudo apt-get update
+sudo apt-cache madison docker-ce docker-ce-cli
+
+# Create json config for docker:
+
+```bash
+sudo mkdir -p /etc/docker/
+cat <<EOF >daemon.json
+{
+    "dns": ["8.8.8.8", "8.8.4.4"],
+    "iptables": false
+}
+EOF
+sudo mv -f daemon.json /etc/docker/daemon.json
+
+# Install docker:
+
+sudo apt-mark unhold docker-ce
+export DOCKER_VERSION=5:18.09.5~3-0~ubuntu-xenial
+sudo apt-get install docker-ce=$DOCKER_VERSION docker-ce-cli=$DOCKER_VERSION containerd.io
+sudo apt-mark hold docker-ce
+
+# Note 1: mark on hold to prevent auto upgrades
+# Note 2: docker-ce-cli appeared >= 18.09
+
+```bash
+systemctl daemon-reload
+systemctl status docker
+```
 
 # add current user to docker group
 sudo usermod -aG docker $USER
 
-# allow env vars to be passed to docker, fix for systemd
-sudo mkdir -p /etc/systemd/system/docker.service.d
 
-cat <<\EOF > systemd_docker.conf
-[Service]
-EnvironmentFile=-/etc/default/docker
-ExecStart=
-ExecStart=/usr/bin/dockerd -H fd:// $DOCKER_OPTS
-EOF
-sudo cp systemd_docker.conf /etc/systemd/system/docker.service.d/
-
-sudo sed -i 's/^#DOCKER_OPTS=/DOCKER_OPTS=/' /etc/default/docker
-sudo sed -i 's/^DOCKER_OPTS=.*$/DOCKER_OPTS="--dns 8.8.8.8 --dns 8.8.4.4 --iptables=false"/' /etc/default/docker
-sudo systemctl daemon-reload
-sudo systemctl restart docker
 
 # install docker-compose
-sudo curl -L https://github.com/docker/compose/releases/download/1.8.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose.t && sudo chmod +x /usr/local/bin/docker-compose.t && sudo mv /usr/local/bin/docker-compose.t /usr/local/bin/docker-compose
+# sudo curl -L https://github.com/docker/compose/releases/download/1.8.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose.t && sudo chmod +x /usr/local/bin/docker-compose.t && sudo mv /usr/local/bin/docker-compose.t /usr/local/bin/docker-compose
 
 # install docker completion for bash
-sudo curl -L https://raw.githubusercontent.com/docker/compose/$(docker-compose version --short)/contrib/completion/bash/docker-compose -o /etc/bash_completion.d/docker-compose
+# sudo curl -L https://raw.githubusercontent.com/docker/compose/$(docker-compose version --short)/contrib/completion/bash/docker-compose -o /etc/bash_completion.d/docker-compose
 
 # http://www.acervera.com/blog/2016/03/05/ufw_plus_docker
 if [ -e /etc/default/ufw ]; then
