b7128e00e95a3d1ced0b02ab844f3987f31f5303
Laurent Monin <github@norz.org>
2021-01-20 08:04:45 +0000

    Only reload if config file changed or forced

--- docker_reload_config.sh
+++ docker_reload_config.sh
@@ -2,10 +2,25 @@
 
 set -ex
 
+FORCE=0
+if [ "$1" = "force" ]; then
+  FORCE=1
+fi
+
 # copy new docker daemon config
 CONFFILE=/etc/docker/daemon.json
-[ -e "$CONFFILE" ] && cp -fv "$CONFFILE" "$CONFFILE.bak"
-cp -fv hetzner_docker_daemon.json "$CONFFILE"
+SRCFILE=hetzner_docker_daemon.json
+
+if [ -e "$CONFFILE" ]; then
+        if [ $FORCE -eq 0 ]; then
+		if cmp -s "$SRCFILE" "$CONFFILE"; then
+   			echo "$SRCFILE and $CONFFILE are identical, exiting..."
+			exit 1
+		fi
+	fi
+ 	cp -fv "$CONFFILE" "$CONFFILE.bak"
+fi
+cp -fv "$SRCFILE" "$CONFFILE"
 
 # use live restore feature
 # https://docs.docker.com/config/containers/live-restore/
@@ -19,6 +34,7 @@ sleep 5
 # Ensure live restore is set, and restart docker
 if docker info 2>/dev/null|grep -q 'Live Restore Enabled: true'; then
   systemctl restart docker.service
+  sleep 5
 fi
 
 systemctl status docker.service
