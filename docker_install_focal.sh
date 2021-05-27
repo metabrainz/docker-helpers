c7a2938986a5a5d99e8d319a740e4f2eb7ce2275
Laurent Monin <github@norz.org>
2021-05-27 20:47:17 +0000

    Go back to iptables=false

--- docker_install_focal.sh
+++ docker_install_focal.sh
@@ -25,14 +25,24 @@ sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io
 
 # default daemon config
-cp -f hetzner_docker_daemon_focal.json /etc/docker/daemon.json
+sudo cp -f hetzner_docker_daemon_focal.json /etc/docker/daemon.json
 
-# install ufw-docker
-sudo wget -O /usr/local/bin/ufw-docker \
-  https://github.com/chaifeng/ufw-docker/raw/master/ufw-docker
-sudo chmod +x /usr/local/bin/ufw-docker
+UFW_DOCKER=no
+if [ "$UFW_DOCKER" = "yes" ]; then
+	sudo cp -f hetzner_docker_daemon_focal_ufw_docker.json /etc/docker/daemon.json
+	# install ufw-docker
+	sudo wget -O /usr/local/bin/ufw-docker \
+	  https://github.com/chaifeng/ufw-docker/raw/master/ufw-docker
+	sudo chmod +x /usr/local/bin/ufw-docker
 
-sudo ufw-docker install
+	sudo ufw-docker install
+	sudo systemctl restart ufw.service
+else
+	sudo ./docker_ufw_rules.sh
+	sudo systemctl restart ufw.service
+fi
 
 # prevent automatic upgrades of docker packages
 sudo apt-mark hold docker-ce docker-ce-cli containerd.io
+
+sudo systemctl restart docker.service
